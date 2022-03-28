16a0d831055d01241ab95a31b6f2ddd10c51aaa6
Sergey <Zismantt@gmail.com>
2022-03-28 17:55:23 +0300

    add object personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let a = 5;
-console.log(a); 
+const numberOfFilms = +prompt('Скільки фільмів ви вже бачили', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Який із останніх побачених фільмів?', ''),
+    b = prompt('На скільки його оціните?', ''),
+    c = prompt('Який із останніх побачених фільмів?', ''),
+    d = prompt('На скільки його оціните?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
